55f541e11b3940720a4889818a25ce931748f4c5
Florian Kapfenberger <florian@kapfenberger.me>
2020-10-05 23:02:08 +0200

    add build command

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    './src/**/*.html',
+  ],
   theme: {
     extend: {
       fontFamily: {
