6451abb865d40f918fe3ff31a0a68dd59c592ce9
Florian Kapfenberger <florian@kapfenberger.me>
2020-10-05 22:58:28 +0200

    add search box logic

--- src/index.js
+++ src/index.js
@@ -12,3 +12,27 @@ const map = new mapkit.Map("apple-maps", {
     center,
     cameraDistance: 15000,
 })
+
+const search = new mapkit.Search({
+    language: navigator.language || navigator.userLanguage,
+    getsUserLocation: true,
+    region: map.region,
+});
+
+const searchFormElement = document.getElementById("search-form");
+const searchBoxElement = document.getElementById("search-box");
+
+searchFormElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const query = searchBoxElement.value;
+    if (query) {
+        search.search(query, (error, data) => {
+            console.log(data);
+            if (error) {
+                console.log("Error", error);
+                return;
+            }
+            map.setRegionAnimated(data.boundingRegion);
+        });
+    }
+});
